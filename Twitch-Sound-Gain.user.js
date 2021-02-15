f033beb5c36d979cc94ca862f08cf3e1a5df6608
Nenka Lab#9485 <nenka@naver.com>
2021-02-15 10:13:53 +0900

    Update Twitch-Sound-Gain.user.js

--- Twitch-Sound-Gain.user.js
+++ Twitch-Sound-Gain.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Twitch-Sound-Gain
 // @namespace   Twitch-Sound-Gain
-// @version     0.0.9
+// @version     0.0.10
 // @author      NenkaLab
 // @description 트위치 비디오 사운드를 증폭 시킵니다. / Amplifies the twitch video sound(?).
 // @icon        https://www.twitch.tv/favicon.ico
@@ -90,9 +90,12 @@ if (window.TWITCH_SOUND_GAIN === undefined) {
         async function updateRoom() {
             room = window.location.pathname;
             abConsole("ROOM"+room);
-            if (audioBoosterValueElement != null) getData("booster_value"+room, 1).then(v => {audioBoosterValueElement.innerText = v+"%"});
-            if (audioBoosterElement != null) getData("booster_value"+room, 1).then(v => {audioBoosterElement.value = v*10});
-            if (abGainNode != null) getData("booster_value"+room, 1).then(v => {abGainNode.gain.value = v});
+            if (audioBoosterValueElement != null)
+                audioBoosterValueElement.innerText = await getData("booster_value"+room, 1)+"%";
+            if (audioBoosterElement != null)
+                audioBoosterElement.value = await getData("booster_value"+room, 1)*10;
+            if (abGainNode != null)
+                abGainNode.gain.value = await getData("booster_value"+room, 1);
         }
 
         async function aBoosterInit() {
