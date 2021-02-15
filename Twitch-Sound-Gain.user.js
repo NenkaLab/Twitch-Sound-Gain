21e2530d6ce904747caf41bf9a7cee85d859d00b
Nenka Lab#9485 <nenka@naver.com>
2021-02-15 10:00:33 +0900

    Update Twitch-Sound-Gain.user.js

--- Twitch-Sound-Gain.user.js
+++ Twitch-Sound-Gain.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Twitch-Sound-Gain
 // @namespace   Twitch-Sound-Gain
-// @version     0.0.5
+// @version     0.0.6
 // @author      NenkaLab
 // @description 트위치 비디오 사운드를 증폭 시킵니다. / Amplifies the twitch video sound(?).
 // @icon        https://www.twitch.tv/favicon.ico
@@ -85,8 +85,17 @@ if (window.TWITCH_SOUND_GAIN === undefined) {
         var abSource;
         var abGainNode;
         var targetVideo;
-        var room = window.location.pathname;
-        abConsole("ROOM"+room);
+        var room;
+
+        var pushState = history.pushState;
+        history.pushState = function () {
+            pushState.apply(history, arguments);
+            room = window.location.pathname;
+            abConsole("ROOM"+room);
+            if (audioBoosterValueElement != null) audioBoosterValueElement.innerText = await getData("booster_value"+room, 1)+"%";
+            if (audioBoosterElement != null) audioBoosterElement.value = await getData("booster_value"+room, 1)*10;
+            if (abGainNode != null) abGainNode.gain.value = await getData("booster_value"+room, 1);
+        };
 
         async function aBoosterInit() {
             abConsole("START_INIT");
