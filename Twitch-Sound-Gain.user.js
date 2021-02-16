6e9052cfa7ac04da2a64ad02c2af6656015be9a3
Nenka Lab#9485 <nenka@naver.com>
2021-02-16 16:18:58 +0900

    Update Twitch-Sound-Gain.user.js

--- Twitch-Sound-Gain.user.js
+++ Twitch-Sound-Gain.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Twitch-Sound-Gain
 // @namespace   Twitch-Sound-Gain
-// @version     0.0.13
+// @version     0.0.14
 // @author      NenkaLab
 // @description 트위치 비디오 사운드를 증폭 시킵니다. / Amplifies the twitch video sound(?).
 // @icon        https://www.twitch.tv/favicon.ico
@@ -110,6 +110,7 @@ if (window.TWITCH_SOUND_GAIN === undefined) {
             let roomName = window.location.pathname.split("/");
             switch(roomName[1]) {
                 case 'video':
+                case 'videos':
                     room = "="+document.querySelector(".channel-info-content .tw-align-items-center.tw-flex > a > h1").parentElement.getAttribute("href").substring(1);
                     break;
                 default:
